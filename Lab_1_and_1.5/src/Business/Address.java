055305b653731a20e478e72ae99d0d6f490abcb1
Satwik <kashyap.sa@husky.neu.edu>
2019-02-03 12:44:43 -0500

    lab assignment 2 and 1.5

---
+++ Lab_1_and_1.5/src/Business/Address.java
@@ -0,0 +1,57 @@
+
+package Business;
+
+public class Address 
+{
+    String  Address_street_line_1,Address_street_line_2,Address_city,Address_state,Address_zip_code,Address_country;
+
+    public String getAddress_street_line_1() {
+        return Address_street_line_1;
+    }
+
+    public void setAddress_street_line_1(String Address_street_line_1) {
+        this.Address_street_line_1 = Address_street_line_1;
+    }
+
+    public String getAddress_street_line_2() {
+        return Address_street_line_2;
+    }
+
+    public void setAddress_street_line_2(String Address_street_line_2) {
+        this.Address_street_line_2 = Address_street_line_2;
+    }
+
+    public String getAddress_city() {
+        return Address_city;
+    }
+
+    public void setAddress_city(String Address_city) {
+        this.Address_city = Address_city;
+    }
+
+    public String getAddress_state() {
+        return Address_state;
+    }
+
+    public void setAddress_state(String Address_state) {
+        this.Address_state = Address_state;
+    }
+
+    public String getAddress_zip_code() {
+        return Address_zip_code;
+    }
+
+    public void setAddress_zip_code(String Address_zip_code) {
+        this.Address_zip_code = Address_zip_code;
+    }
+
+    public String getAddress_country() {
+        return Address_country;
+    }
+
+    public void setAddress_country(String Address_country) {
+        this.Address_country = Address_country;
+    }
+    
+
+}
